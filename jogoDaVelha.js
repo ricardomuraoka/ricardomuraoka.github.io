e68efac0b5f4c7888cc7eb4d5f1e973aff87723f
Ricardo <ricardomr8@hotmail.com>
2022-10-15 11:02:21 -0300

    Adding the first idea on printing and adding values to the game.

--- jogoDaVelha.js
+++ jogoDaVelha.js
@@ -7,3 +7,16 @@ Adicionais (opcional)
 Permita que o jogador possa "voltar atrás" em qualquer número de jogadas
 Permita também o modo jogador vs. computador */
 
+let jogoDaVelha = new Array();
+let item1 = [" 1 ", " 2 ", " 3 "],
+    item2 = [" 4 ", " 5 ", " 6 "],
+    item3 = [" 7 ", " 8 ", " 9 "];
+
+jogoDaVelha.push(item1);
+jogoDaVelha.push(item2);
+jogoDaVelha.push(item3);
+
+for(let i = 0; i < jogoDaVelha.length; i++)
+{
+    console.log(jogoDaVelha[i][0] + ' | ' + jogoDaVelha[i][1] + ' | ' + jogoDaVelha[i][2]);
+}
