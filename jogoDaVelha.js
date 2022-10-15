52dfc15b23cd8cf73d37820c5f98b18f356ba8e0
Ricardo <ricardomr8@hotmail.com>
2022-10-15 11:45:04 -0300

    Adding the first idea on printing and adding values to the game.

--- jogoDaVelha.js
+++ jogoDaVelha.js
@@ -6,7 +6,7 @@ Adicionais (opcional)
 Permita que o jogador possa "voltar atrás" em qualquer número de jogadas
 Permita também o modo jogador vs. computador */
 
-let jogoDaVelha = new Array();
+let jogoDaVelha = [];
 let item1 = [" 1 ", " 2 ", " 3 "],
     item2 = [" 4 ", " 5 ", " 6 "],
     item3 = [" 7 ", " 8 ", " 9 "];
@@ -19,3 +19,9 @@ for(let i = 0; i < jogoDaVelha.length; i++)
 {
     console.log(jogoDaVelha[i][0] + ' | ' + jogoDaVelha[i][1] + ' | ' + jogoDaVelha[i][2]);
 }
+
+let p1Move = function () {
+    return ("Digite o número correspondente para realizar a sua jogada | 1 a 9 ");
+}
+
+console.log(p1Move())
---
+++ jogoDaVelha2.js
@@ -0,0 +1,25 @@
+class JogoDaVelha {
+    posicao1;
+    posicao2;
+    posicao3;
+    posicao4;
+    posicao5;
+    posicao6;
+    posicao7;
+    posicao8;
+    posicao9;
+
+    constructor(posicao1, posicao2, posicao3, posicao4, posicao5, posicao6, posicao7, posicao8, posicao9) {
+        this.posicao1 = posicao1;
+        this.posicao2 = posicao2;
+        this.posicao3 = posicao3;
+        this.posicao4 = posicao4;
+        this.posicao5 = posicao5;
+        this.posicao6 = posicao6;
+        this.posicao7 = posicao7;
+        this.posicao8 = posicao8;
+        this.posicao9 = posicao9;
+    }
+
+
+}
