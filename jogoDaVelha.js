c70ed55fdfb6eb87950c38d00dfcb6285ea322b2
Ricardo <ricardomr8@hotmail.com>
2022-10-15 09:25:38 -0300

    Initial commit

---
+++ jogoDaVelha.js
@@ -0,0 +1,9 @@
+/* Regras
+
+A cada rodada, imprima o tabuleiro e pergunte a posição que o jogador deseja jogar
+No final do jogo, deve indicar qual jogador ganhou ou se deu velha
+Adicionais (opcional)
+
+Permita que o jogador possa "voltar atrás" em qualquer número de jogadas
+Permita também o modo jogador vs. computador */
+
